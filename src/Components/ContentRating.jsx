23b6acd75a54b38e1456c4aca80cdfd88d3958e1
TMS <tms@huhuhu.hu>
2025-02-23 09:22:56 -0500

    ContentRating.jsx file updated

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,42 +1,41 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
-
 class ContentRating extends Component {
   constructor() {
     super();
     this.state = {
-        likes: 0;
-        dislikes: 0;
-        HandleLike: () => {
-            this.setState((prevState) => ({
-                likes: prevState.likes + 1
-            }));
-        };
-        HandleDislike: () => {
-            this.setState((prevState) => ({
-                dislikes: prevState.dislikes + 1
-            }));
-        }
-    };
+        likes: 0,
+        dislikes: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+            likes: prevState.likes + 1
+          }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+            dislikes: prevState.dislikes + 1
+          }));
+      }
+      };
   }
   render() {
     return (
+     <>
+     <h1>Text Content Rating</h1>
      <div className='content-rating'>
-        <p>
-            //Add text here
+        <p>If you like this random text, click the "like" button please. If not, leave this site please, and do not event try to click the "dislike" button.
         </p>
         <div className='rating-buttons'>
-            <button className='like-button'>
-                Like ({this.state.likes})
-            </button>
-            <button clasName='dislike-button'>
-                Dislike ({this.state.dislikes})
-            </button>
+        <button className="like-button" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
         </div>
      </div>
+     </>
     );
   }
 }
-
 export default ContentRating;
