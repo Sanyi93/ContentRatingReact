5b6b53a1781cfc597e1407fbb6edce9c618ae4ea
TMS <tms@huhuhu.hu>
2025-02-23 09:44:39 -0500

    totalRating changed

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -9,19 +9,16 @@ class ContentRating extends Component {
         totalRating: 0,
       handleLike:() => {
         this.setState((prevState) => ({
-            likes: prevState.likes + 1
+            likes: prevState.likes + 1,
+            totalRating: prevState.totalRating +1
           }));
       },
       handleDislike:() => {
         this.setState((prevState) => ({
-            dislikes: prevState.dislikes + 1
+            dislikes: prevState.dislikes + 1,
+            totalRating: prevState.totalRating +1
           }));
       },
-      handleTotalRating:() => {
-        this.setState((prevState) => ({
-            totalRating: prevState.totalRating +1
-        }));
-      }
       };
   }
   render() {
@@ -32,10 +29,10 @@ class ContentRating extends Component {
         <p>If you like this random text, click the "like" button please. If not, leave this site please, and do not event try to click the "dislike" button.
         </p>
         <div className='rating-buttons'>
-        <button className="like-button" onClick={[this.state.handleLike, this.state.handleTotalRating]}>
+        <button className="like-button" onClick={this.state.handleLike}>
             Like ({this.state.likes})
           </button>
-          <button className="dislike-button" onClick={[this.state.handleDislike, this.state.handleTotalRating]}>
+          <button className="dislike-button" onClick={this.state.handleDislike}>
             Dislike ({this.state.dislikes})
           </button>
           <p>Total Ratings: ({this.state.totalRating})</p>
